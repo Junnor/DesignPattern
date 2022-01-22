d872656be67e3e0d60ba51987d64ecd9bb0972f2
dq <dengquanzhu@gmail.com>
2022-01-22 10:40:44 +0800

    fix

---
+++ Factory.swift
@@ -0,0 +1,29 @@
+//
+//  Factory.swift
+//  DesignPattern
+//
+//  Created by dq on 2022/1/22.
+//
+
+import UIKit
+
+enum PhoneType {
+    case iPhone
+    case XiaoMi
+    case HuaWei
+}
+
+struct Factory {
+    
+    static func createPhone(type: PhoneType) -> Phone {
+        switch type {
+        case .iPhone:
+            return IPhoneProduct()
+        case .XiaoMi:
+            return XiaomiProduct()
+        case .HuaWei:
+            return HuaWeiProduct()
+        }
+    }
+    
+}
---
+++ Phone.swift
@@ -0,0 +1,31 @@
+//
+//  Phone.swift
+//  DesignPattern
+//
+//  Created by dq on 2022/1/22.
+//
+
+import Foundation
+
+protocol Phone {
+    func create()
+}
+
+
+class IPhoneProduct: Phone {
+    func create() {
+        print("Created iPhone")
+    }
+}
+
+class XiaomiProduct: Phone {
+    func create() {
+        print("Created XiaoMi")
+    }
+}
+
+class HuaWeiProduct: Phone {
+    func create() {
+        print("Created HuaWei")
+    }
+}
