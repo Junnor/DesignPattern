394626d4183feffc254132c08eddbd9e9490c2ac
dq <dengquanzhu@gmail.com>
2022-01-22 14:17:52 +0800

    fix

--- ViewController.swift
+++ ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        
     
         let user = MasterUser.shared
         user.name = "Zhu"
